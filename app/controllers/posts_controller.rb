a95228ee55500abda23f9444b2099b93b7da31c6
fuchimo <f.masaki.1988@gmail.com>
2021-02-08 12:08:30 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,13 @@
 class PostsController < ApplicationController
 
   def index
-    @post = Post.find(1)
-    @posts = Post.all
-  end
-
-  def new
+    #@post = Post.find(1)
+    @posts = Post.all.order(id: "DESC")
   end
 
   def create
-    @content = params[:content]
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 
 end
